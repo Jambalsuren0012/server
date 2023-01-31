7e484bece44798db39077062df656fb597860dc5
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-01-31 17:15:48 +0800

    server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require ("express")
 const cors = require ("cors");
 const fs = require ("fs");
 const { json } = require("express");
+const {v4:uuidv4}= require("uuid");
+const bcrypt = require("bcrypt")
 
 
-const port =8000;
+const port =8001;
 
 const server = express ()
 server.use(cors());
@@ -14,25 +16,44 @@ server.get ("/",(req, res) => {
     res.status(200).json({message:"hello Express server"});
 });
 
-server.post("/signup",(req,res)=>{
-    const{name,role} = req.body;
-    fs.readFile("users.json", "utf-8", (err, data)=>{
-        if(err){
-            console.log("Can't read file!!!");
-            return;
-        }
-        console.log(data)
-        const parsedData = JSON.parse(data);
-        const newUser ={name, role, id:parsedData.users.length}
-        parsedData.users.push(newUser);
-        fs.writeFile("users.json", JSON.stringify(parsedData),(err)=>{
-            if (err){
-                res.status(400).json({message:"Error"});
-            }
-            res.status(201).json({message:"Created new user"})
-        })
-    })
-})
+server.post("/signup", (req, res)=> {
+    const {name, role,email, password} = req.body;
+    const data = fs.readFileSync("users.json","utf-8");
+    const parsedData = JSON.parse(data);
+    const id = uuidv4();
+    const salted = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password,salted);
+    console.log(hashedPassword);
+    const newUser = {
+        id,
+        name,
+        role,
+        email,
+        password: hashedPassword
+    };
+    parsedData.users.push(newUser);
+    fs.writeFileSync("users.json", JSON.stringify(parsedData));
+    res.status(201).json({message: "Shine hereglegch amjilttai burtgegdlee."})
+});
+
+server.post("/signin", (req, res) => {
+    const { id, email, password } = req.body;
+    const data = fs.readFileSync("users.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    const findUser = parsedData.users.find((user) => user.id === id);
+    if (!findUser) {
+      res.status(401).json({ message: "Ийм хэрэглэгч олдсонгүй" });
+    }
+  
+    const isCheck = bcrypt.compareSync(password, findUser.password);
+    if (isCheck) {
+      res.status(200).json({ message: "Амжилттай нэвтэрлээ.", user: findUser });
+    } else {
+      res
+        .status(401)
+        .json({ message: "Имэйл эсвэл нууц үг буруу байна.", user: null });
+    }
+  });
 
 server.get("/users",(req, res)=>{
     fs.readFile("users.json","utf-8",(err, data)=>{
@@ -46,14 +67,38 @@ server.get("/users",(req, res)=>{
     });
 });
 
-// server.get("/users",(req,res)=> {
-//     res.status(201).json({users})
-// });
+
 server.get("/user/:id", (req, res) => {
     const {id} = req.params;
-    const user = users.find((el)=> el.id === id);
+    const data = fs.readFileSync("users.json", "utf-8")
+    const parsedData = JSON.parse(data);
+    const user = parsedData.users.find((el)=> el.id === id);
     res.status(200).json({user});
 });
+
+server.put("./users/:id", (req, res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const data = fs.readFileSync("users.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    const findIndex =parsedData.users.findIndex((el)=> el.id ===id);
+    parsedData.users[findIndex].name = name;
+    fs.writeFileSync("users.json", JSON.stringify(parsedData));
+    res
+    .status(201)
+    .json({message:"shine hereglegchiin ogogdol amjilttai solilgdloo"})
+});
+server.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const data = fs.readFileSync("users.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    const findIndex = parsedData.users.findIndex((el) => el.id === id);
+    parsedData.users.splice(findIndex, 1);
+    fs.writeFileSync("users.json", JSON.stringify(parsedData));
+    res
+      .status(201)
+      .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+  });
 
 
 server.listen (port, () => {
